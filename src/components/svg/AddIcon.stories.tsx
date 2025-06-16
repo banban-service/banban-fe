0518293c9b5b4351e642012d0c86d34c73fd83c6
seolbotdae <seolbotdae@gmail.com>
2025-06-16 19:55:19 +0900

    feat: 아이콘 컴포넌트 추가

---
+++ src/components/svg/AddIcon.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { AddIcon } from '@/components/svg';
+
+const meta: Meta<typeof AddIcon> = {
+  title: 'Icons/AddIcon',
+  component: AddIcon,
+  argTypes: {
+    width: { control: { type: 'number', min: 8, max: 128 } },
+    height: { control: { type: 'number', min: 8, max: 128 } },
+  },
+  args: {
+    width: 32,
+    height: 32,
+    color: "#fff"
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof AddIcon>;
+
+export const Default: Story = {
+  args: {
+    color: "#000000",
+  }
+};
---
+++ src/components/svg/CloseIcon.tsx
@@ -0,0 +1,29 @@
+import { forwardRef } from 'react';
+import type { IconProps } from '@/types/IconProps';
+
+export const CloseIcon = forwardRef<SVGSVGElement, IconProps>(
+  ({ width = 32, height = 32, color, ...props }, ref) => {
+    return (
+      <svg
+        ref={ref}
+        width={width}
+        height={height}
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ color: color }}
+        {...props}
+      >
+        <path 
+          d="M2.58579 2.58579C3.36684 1.80474 4.63316 1.80474 5.41421 2.58579L29.4142 26.5858C30.1953 27.3668 30.1953 28.6332 29.4142 29.4142C28.6332 30.1953 27.3668 30.1953 26.5858 29.4142L2.58579 5.41421C1.80474 4.63316 1.80474 3.36683 2.58579 2.58579Z"
+          fill="currentColor"
+        />
+        <path 
+          d="M29.4142 2.58579C28.6332 1.80474 27.3668 1.80474 26.5858 2.58579L2.58579 26.5858C1.80474 27.3668 1.80474 28.6332 2.58579 29.4142C3.36684 30.1953 4.63317 30.1953 5.41422 29.4142L29.4142 5.41421C30.1953 4.63316 30.1953 3.36683 29.4142 2.58579Z"
+          fill="currentColor"
+        />
+      </svg>
+    )
+  }
+);
+
+CloseIcon.displayName = 'CloseIcon';
