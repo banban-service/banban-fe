ab536688d10974fb4529346d72553f88b5a6e95a
CEJSH <eun2763@naver.com>
2025-06-26 22:06:59 +0900

    chore: LoginButton 컴포넌트 스토리 작성

---
+++ src/components/auth/LoginPage/LoginButton/LoginButton.stories.tsx
@@ -0,0 +1,39 @@
+// src/components/LoginButton/LoginButton.stories.tsx
+import React from "react";
+import { LoginButton } from "./LoginButton";
+import Image from "next/image";
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+const meta: Meta<typeof LoginButton> = {
+  title: "Components/LoginButton",
+  component: LoginButton,
+  argTypes: {
+    color: { control: "color" },
+    fontcolor: { control: "color" },
+    disabled: { control: "boolean" },
+    children: { control: "text" },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof LoginButton>;
+
+export const Default: Story = {
+  args: {
+    color: "#F9E000",
+    fontcolor: "#371C1D",
+    children: "카카오로 로그인",
+    icon: <Image src="/kakao.png" alt="kakao-icon" width={24} height={24} />,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    color: "#F9E000",
+    fontcolor: "#371C1D",
+    disabled: true,
+    children: "카카오로 로그인",
+    icon: <Image src="/kakao.png" alt="kakao-icon" width={24} height={24} />,
+  },
+};
