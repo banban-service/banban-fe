266865d4d998724768532f778e428d33d207ffef
seolbotdae <seolbotdae@gmail.com>
2025-07-27 18:17:17 +0900

    style: 코드 포맷팅 개선 및 가독성 향상

--- src/api/getFeeds.ts
+++ src/api/getFeeds.ts
@@ -2,10 +2,16 @@ import type { FeedsRequest, FeedsResponse } from "@/types/feeds";
 import { getMockFeeds } from "@/mock/actions/feeds";
 import camelcaseKeys from "camelcase-keys";
 
-const getFeeds = async ({ lastId, size }: FeedsRequest): Promise<FeedsResponse> => {
+const getFeeds = async ({
+  lastId,
+  size,
+}: FeedsRequest): Promise<FeedsResponse> => {
   const res = await getMockFeeds({ last_id: lastId, size: size });
 
-  const convertedRes = camelcaseKeys(res as unknown as Record<string, unknown>, { deep: true });
+  const convertedRes = camelcaseKeys(
+    res as unknown as Record<string, unknown>,
+    { deep: true },
+  );
   return convertedRes as unknown as FeedsResponse;
 };
 
