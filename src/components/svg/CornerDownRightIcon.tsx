c8c2c1c8697f4388077ddcd68b2486df4fc569a8
seolbotdae <seolbotdae@gmail.com>
2025-07-28 17:44:55 +0900

    fix: CornerDownRightIcon에서 stroke 속성의 대시 표기법을 camelCase로 수정

--- src/components/svg/CornerDownRightIcon.tsx
+++ src/components/svg/CornerDownRightIcon.tsx
@@ -18,8 +18,8 @@ const CornerDownRightIcon = ({
       <path
         d="M15 10L20 15M20 15L15 20M20 15H8C6.93913 15 5.92172 14.5786 5.17157 13.8284C4.42143 13.0783 4 12.0609 4 11V4"
         stroke={color}
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
