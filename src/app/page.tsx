4d009e278850c4ae698725becbc09b854b1b60c3
CEJSH <eun2763@naver.com>
2025-08-02 14:20:22 +0900

    feat: 로그인 버튼 클릭시 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,22 @@ import Header from "@/components/layout/Header";
 import LeftSection from "@/components/layout/LeftSection/LeftSection";
 import RightSection from "@/components/layout/RightSection/RightSection";
 import useAuth from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { isLoggedIn } = useAuth();
-
+  const router = useRouter();
   return (
     <div className="relative mx-auto w-fit">
-      {<Header isLoggedIn={isLoggedIn} isNew />}
+      {
+        <Header
+          isLoggedIn={isLoggedIn}
+          isNew
+          onLogin={() => {
+            router.push("/login");
+          }}
+        />
+      }
       <div className="flex gap-6 pt-[64px] h-[100dvh]">
         <LeftSection />
         <RightSection />
